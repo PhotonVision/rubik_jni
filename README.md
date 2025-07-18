de1742748f0f3e9443491844b4d74886a275ed73
Sam948-byte <samf.236@proton.me>
2025-07-17 22:23:38 -0500

    git ignore added

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-This is a Java wrapper for object detection on the [Rubik Pi 3 Platform](https://www.thundercomm.com/rubik-pi-3/en/)
+IN PROGRESS
--- src/main/java/org/photonvision/rubik/RubikJNI.java
+++ src/main/java/org/photonvision/rubik/RubikJNI.java
@@ -15,78 +15,87 @@
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 
- package org.photonvision.rubik;
+package org.photonvision.rubik;
 
- import org.opencv.core.Point;
- import org.opencv.core.Rect2d;
- 
- public class RubikJNI {
+import org.opencv.core.Point;
+import org.opencv.core.Rect2d;
 
- 
-     public static class RubikResult {
-         public RubikResult(
-             int left, int top, int right, int bottom, float conf, int class_id
-         ) {
-             this.conf = conf;
-             this.class_id = class_id;
-             this.rect = new Rect2d(new Point(left, top), new Point(right, bottom));
-         }
-         
-         public final Rect2d rect;
-         public final float conf;
-         public final int class_id;
- 
-         @Override
-         public String toString() {
-             return "RubikResult [rect=" + rect + ", conf=" + conf + ", class_id=" + class_id + "]";
-         }
- 
-         @Override
-         public int hashCode() {
-             final int prime = 31;
-             int result = 1;
-             result = prime * result + ((rect == null) ? 0 : rect.hashCode());
-             result = prime * result + Float.floatToIntBits(conf);
-             result = prime * result + class_id;
-             return result;
-         }
- 
-         @Override
-         public boolean equals(Object obj) {
-             if (this == obj)
-                 return true;
-             if (obj == null)
-                 return false;
-             if (getClass() != obj.getClass())
-                 return false;
-             RubikResult other = (RubikResult) obj;
-             if (rect == null) {
-                 if (other.rect != null)
-                     return false;
-             } else if (!rect.equals(other.rect))
-                 return false;
-             if (Float.floatToIntBits(conf) != Float.floatToIntBits(other.conf))
-                 return false;
-             if (class_id != other.class_id)
-                 return false;
-             return true;
-         }
-     }
- 
-     /**
-      * TODO: Determine what needs to go here based on native code
-      */
-     public static native long create();
-     
-     /**
-      * Delete all native resources assocated with a detector
-      TODO: Determine what needs to go here based on native code
-      */
-     public static native long destroy();
- 
-     /**
-      * Run detction
-    TODO: Determine what needs to go here based on native code
-      */
-     public static native RubikResult[] detect();
- }
+public class RubikJNI {
+
+    /**
+     * A class representing the result of a detection.
+     */
+    public static class RubikResult {
+        public RubikResult(
+                int left, int top, int right, int bottom, float conf, int class_id) {
+            this.conf = conf;
+            this.class_id = class_id;
+            this.rect = new Rect2d(new Point(left, top), new Point(right, bottom));
+        }
+
+        public final Rect2d rect;
+        public final float conf;
+        public final int class_id;
+
+        @Override
+        public String toString() {
+            return "RubikResult [rect=" + rect + ", conf=" + conf + ", class_id=" + class_id + "]";
+        }
+
+        @Override
+        public int hashCode() {
+            final int prime = 31;
+            int result = 1;
+            result = prime * result + ((rect == null) ? 0 : rect.hashCode());
+            result = prime * result + Float.floatToIntBits(conf);
+            result = prime * result + class_id;
+            return result;
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            if (this == obj)
+                return true;
+            if (obj == null)
+                return false;
+            if (getClass() != obj.getClass())
+                return false;
+            RubikResult other = (RubikResult) obj;
+            if (rect == null) {
+                if (other.rect != null)
+                    return false;
+            } else if (!rect.equals(other.rect))
+                return false;
+            if (Float.floatToIntBits(conf) != Float.floatToIntBits(other.conf))
+                return false;
+            if (class_id != other.class_id)
+                return false;
+            return true;
+        }
+    }
+
+    /**
+     * Create a RubikJNI instance with the specified model path.
+     * 
+     * @param modelPath
+     * @return A handle to the RubikJNI instance.
+     */
+    private static native long create(String modelPath);
+
+    /**
+     * Destroy the RubikJNI instance.
+     * 
+     * @param handle
+     */
+    private static native void destroy(long handle);
+
+    /**
+     * Detect in the given image
+     * 
+     * @param handle   The handle to the RubikJNI instance.
+     * @param imagePtr The pointer to the image data.
+     * @return An array of {@link RubikJNI.RubikResult} objects containing the
+     *         detection results.
+     */
+    private static native RubikResult[] detect(long handle, long imagePtr);
+}
