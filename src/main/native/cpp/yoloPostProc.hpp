b8c7717e4f66bc981ddbf6570139fb698c6395b1
samfreund <samf.236@proton.me>
2025-12-18 16:37:16 -0600

    refactor post-proc

---
+++ src/main/native/cpp/yoloPostProc.hpp
@@ -0,0 +1,39 @@
+/*
+ * Copyright (C) Photon Vision.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+#include <vector>
+
+#include <jni.h>
+#include <opencv2/opencv.hpp>
+#include <tensorflow/lite/c/c_api.h>
+
+#include "utils.hpp"
+
+/***
+ * Performs YOLO post-processing including box decoding and NMS.
+ * @param interpreter Pointer to the TensorFlow Lite interpreter.
+ * @param boxThresh Confidence threshold for filtering boxes.
+ * @param nmsThreshold IoU threshold for Non-Maximum Suppression.
+ * @param env Pointer to the JNI environment.
+ * @param input_img Pointer to the input OpenCV image matrix.
+ * @return A JNI array of detection result objects.
+ */
+std::vector<detect_result_t> yoloPostProc(TfLiteInterpreter* interpreter,
+                                          double boxThresh, double nmsThreshold,
+                                          cv::Mat* input_img);
