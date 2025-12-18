2a052efb66c391ae0bd33a576283e55d1fba7396
samfreund <samf.236@proton.me>
2025-12-18 16:13:57 -0600

    fix version for benchmark

--- src/test/java/org/photonvision/rubik/RubikTest.java
+++ src/test/java/org/photonvision/rubik/RubikTest.java
@@ -206,7 +206,7 @@ public class RubikTest {
         System.out.println("Image loaded: " + img.size() + " " + img.type());
 
         System.out.println("Creating Rubik detector");
-        long ptr = RubikJNI.create("src/test/resources/yolov8nCoco.tflite", 0);
+        long ptr = RubikJNI.create("src/test/resources/yolov8nCoco.tflite", 1);
 
         if (ptr == 0) {
             throw new RuntimeException("Failed to create Rubik detector");
